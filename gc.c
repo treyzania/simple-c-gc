d082b2b32c8ba90d84b08845f549b6d66f39aa4c
treyzania <treyzania@gmail.com>
2017-04-26 16:54:37 -0400

    Partially fixed weird counting of refs into other heaps.

--- gc.c
+++ gc.c
@@ -10,20 +10,22 @@ void free_object(oref ref) {
 
 uint64_t __count_references(oref ref, oref* roots, uint64_t root_cnt) {
 
-	// Check to see if all the refs are in the same heap.
-	for (int i = 0; i < root_cnt; i++) {
-		if (ref.heap != roots[i].heap) return -1; // If not then forget it.
-	}
-
 	uint64_t cnt = 0;
 	for (int i = 0; i < root_cnt; i++) {
+
+		if (roots[i].heap != roots[0].heap) continue; // FIXME This won't count references into other heaps.
 
 		otype* t = get_object_type(roots[i]);
 		for (int j = 0; j < t->member_cnt; j++) {
 
 			if (!IS_REFERENCE(roots[i], j)) continue;
 			oref mem = convert_to_oref((void*) (resolve_object(ref) + compute_offset(t, j)), ref.heap);
-			cnt += __count_references(ref, &mem, 1); // Hacky hack.
+
+			if (mem == ref) {
+				cnt++;
+			} else {
+				cnt += __count_references(ref, &mem, 1); // Hacky hack.
+			}
 
 		}
 
